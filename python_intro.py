2002b5a45dcbffdea90e82a71cafe05b5c030aad
grazimorais <grazi.pag@icloud.com>
2022-11-19 18:20:12 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+volume = 80
+if volume < 20: 
+    print("Está um pouco baixo")
+elif 20 <= volume < 40: 
+    print("Está bom para música ambiente")
+elif 40 <= volume < 60: 
+    print("Perfeito, posso ouvir todos os detalhes")
+elif 60 <= volume < 80: 
+    print("Ótimo para festas!")
+elif 80 <= volume < 100: 
+    print("Está muito alto!")
+else: 
+    print("Meus ouvidos doem! :(")
